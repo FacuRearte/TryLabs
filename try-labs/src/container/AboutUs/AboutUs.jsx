d7987381adca440bdb9da3f92b55418ae35a0e95
Sebastian <sebastiandelescabe@gmail.com>
2022-03-10 03:01:50 -0300

    tercer commit

--- try-labs/src/container/AboutUs/AboutUs.jsx
+++ try-labs/src/container/AboutUs/AboutUs.jsx
@@ -5,7 +5,7 @@ import { images } from '../../constants'
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div className='app__aboutus app__bg flex__center' id="about">
+  <div className='app__aboutus app__bg flex__center ' id="about">
     {/* <div className='app__aboutus-overlay flex__center'>
       <img src={images.G} alt="g letter" />
     </div> */}
@@ -15,8 +15,13 @@ const AboutUs = () => (
         <h1 className='headtext__cormorant'>Quiénes somos</h1>
         <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
       </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
       <div className='app__aboutus-content_history' >
-        <h1 className='headtext__cormorant'>Our History Us</h1>
+        <h1 className='headtext__cormorant'>Por qué elegirnos</h1>
         <p className='p__opensans'>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
       </div>
 
