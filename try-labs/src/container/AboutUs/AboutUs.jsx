b77607f6a4d1855b978dec7253a91885fa653830
FacuRearte <facundorearte13@gmail.com>
2022-03-14 20:56:12 -0300

    finish frist part

--- try-labs/src/container/AboutUs/AboutUs.jsx
+++ try-labs/src/container/AboutUs/AboutUs.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import { images } from '../../constants'
+import { images } from "../../constants";
 
-import './AboutUs.css';
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div className='app__aboutus app__bg flex__center section__padding' id="about">
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
     {/* <div className='app__aboutus-overlay flex__center'>
       <img src={images.G} alt="g letter" />
     </div> */}
-    <div className='app__aboutus-content flex__center'>
-
-      <div className='app__aboutus-content_about'>
-        <h1 className='headtext__cormorant'>Quiénes somos</h1>
-        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">¿Quiénes somos?</h1>
+        <p className="p__opensans">
+          Somos una desarrolladora de software, capaces de crear la página que
+          necesita tu empresa en poco tiempo y con las mejores tecnologías del
+          mercado. Adaptándonos a sus necesidades y preferencias, para ofercer
+          el mejor servicio posible.
+        </p>
       </div>
 
-      <div className='app__aboutus-content_knife flex__center'>
+      <div className="app__aboutus-content_knife flex__center">
         <img src={images.knife} alt="about_knife" />
       </div>
 
-      <div className='app__aboutus-content_history' >
-        <h1 className='headtext__cormorant'>Por qué elegirnos</h1>
-        <p className='p__opensans'>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">¿Por qué elegirnos?</h1>
+        <p className="p__opensans">
+          En TryLabs Solutions, ofrecemos un producto adaptable a su presupuesto
+          y tiempo disponible, al precio más competitivo del mercado, siempre
+          manteniendo una relación estrecha entre nuestros desarrolladores y el
+          cliente, para que el producto sea de calidad excelente y el cliente
+          esté satisfecho con el trabajo realizado.
+        </p>
       </div>
-
+      
     </div>
   </div>
 );
--- try-labs/src/container/Header/Header.jsx
+++ try-labs/src/container/Header/Header.jsx
@@ -6,9 +6,9 @@ import './Header.css';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id="home">
     <div className='app__wrapper_info'>
-      <SubHeading title="Hay bala" />
+      <SubHeading title="Desarrolladora de software" />
       <h1 className='app__header-h1'>Soluciones digitales</h1>
-      <p className='p__opensans' style={{ margin: '2rem 0' }}>Somos una empresa fundada en Argentina con amplia experencia en el rubro, Nuestro compromiso es hacer realidad los proyectos de nuestros clientes en tiempo y forma.</p>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>Somos una empresa fundada en Argentina con amplia experencia en el rubro, nuestro compromiso es hacer realidad los proyectos de nuestros clientes en tiempo y forma.</p>
       <a href='#about' className='custom__button'>Conocenos</a>
     </div>
     <div className='app__wrapper_img'>
